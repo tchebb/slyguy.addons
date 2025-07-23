8b77037b6809910947357129b3d4f8e176ad5977
Matt Huisman <hi@matthuisman.nz>
2025-07-24 06:49:13 +1200

    plugin.video.tubed -> v99.2.25

--- plugin.video.tubed/default.py
+++ plugin.video.tubed/default.py
@@ -1,11 +1,16 @@
 import sys
 import xbmc, xbmcaddon, xbmcplugin, xbmcgui
 
+try:
+    from urllib import quote  # Python 2
+except ImportError:
+    from urllib.parse import quote  # Python 3
+
 trailer_addon_id = 'slyguy.trailers'
 addon_id = xbmcaddon.Addon().getAddonInfo('id')
 
 url = sys.argv[0] + sys.argv[2]
-new_url = 'plugin://slyguy.trailers/redirect/?url=' + url
+new_url = 'plugin://slyguy.trailers/redirect/?url=' + quote(url)
 xbmc.log("{} - Re-routing {} -> {}".format(addon_id, url, new_url))
 
 li = xbmcgui.ListItem(path=new_url)
